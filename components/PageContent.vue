444043255c0f74ee7912953935e5f33bc6476267
Gustavo Melo <gustavobarbosa-18@hotmail.com>
2022-05-15 18:57:22 -0300

    feat: list user UI

---
+++ components/PageContent.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="section">
+    <h2 class="title is-3 has-text-grey">
+      {{ title }}  <b-icon
+        :icon="icon"
+        size="is-large"
+      />
+    </h2>
+    <h3 v-if="description.length" class="subtitle is-6 has-text-grey">
+      {{ description }}
+    </h3>
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PageContent',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      default: '',
+      required: false
+    },
+    icon: {
+      type: String,
+      default: '',
+      required: false
+    }
+
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,79 @@
 <template>
-  <div />
+  <PageContent title="Listagem de Usuários" icon="group-account">
+    <BButton class="block is-primary" icon-right="plus">
+      Usuário
+    </BButton>
+    <BTable
+      :data="data"
+      :focusable="true"
+    >
+      <BTableColumn v-slot="props" field="name" label="Nome" searchable>
+        {{ props.row.name }}
+      </BTableColumn>
+      <BTableColumn v-slot="props" field="cpf" label="CPF" searchable>
+        {{ props.row.cpf }}
+      </BTableColumn>
+      <BTableColumn v-slot="props" field="mail" label="E-mail" searchable>
+        {{ props.row.mail }}
+      </BTableColumn>
+      <BTableColumn v-slot="props" field="phone" label="Telefone" searchable>
+        {{ props.row.phone }}
+      </BTableColumn>
+      <BTableColumn v-slot="props" field="actions" label="Ações">
+        <div class="columns is-mobile">
+          <a href="#editar" class="column is-half" @click.prevent="alert(props.row.cpf)">
+            <BIcon icon="pencil-outline" />
+          </a>
+          <a href="#excluir" class="column is-half" @click.prevent="alert(props.row.cpf)">
+            <BIcon icon="delete-outline" />
+          </a>
+        </div>
+      </BTableColumn>
+    </BTable>
+  </PageContent>
 </template>
 
 <script>
 
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      data: [
+        { name: 'Jesse', mail: 'Jesse@test.com.br', phone: '11987654321', cpf: '11244545784' },
+        { name: 'John', mail: 'John@test.com.br', phone: '11987654321', cpf: '05578411244' },
+        { name: 'Tina', mail: 'Tina@test.com.br', phone: '11987654321', cpf: '54112445784' },
+        { name: 'Clarence', mail: 'Clarence@test.com.br', phone: '11987654321', cpf: '04112445784' },
+        { name: 'Anne', mail: 'Anne@test.com.br', phone: '11987654321', cpf: '78404112445' }
+      ],
+      columns: [
+        {
+          field: 'name',
+          label: 'Nome',
+          searchable: true
+        },
+        {
+          field: 'cpf',
+          label: 'CPF',
+          searchable: true
+        },
+        {
+          field: 'mail',
+          label: 'E-mail',
+          searchable: true
+        },
+        {
+          field: 'phone',
+          label: 'Telefone',
+          searchable: true
+        }
+      ]
+    }
+  },
+  methods: {
+    alert (text) {
+      alert(text)
+    }
+  }
 }
 </script>
